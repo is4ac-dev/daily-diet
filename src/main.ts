fc7c221c06c030c1ec492f56b0cacf9f2c6b1076
Isaac Monteiro <isaacmonteiro.alcans@gmail.com>
2026-08-13 11:19:39 -0300

    Aplicando handler global para exceptions do ZOD

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
 import fastify from "fastify";
+import { ZodError } from "zod";
 import { userRoutes } from "./routes/user.js";
 
 const app = fastify();
+
+app.setErrorHandler((error, request, response) => {
+  if (error instanceof ZodError) {
+    return response.status(400).send({
+      statusCode: 400,
+      error: "Bad Request",
+      message: "Invalid request data",
+      issues: error.issues,
+    });
+  }
+
+  request.log.error(error);
+
+  return response.status(500).send({
+    statusCode: 500,
+    error: "Internal Server Error",
+    message: "An unexpected error occurred",
+  });
+});
 
 app.register(userRoutes, {
   prefix: "/user",
